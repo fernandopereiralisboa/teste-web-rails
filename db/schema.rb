36f9b3412c69183f017526228b1f09e1a85d90ae
Fernando Pereira Lisboa <fernandopereiralisboa@gmail.com>
2018-02-21 15:40:49 -0300

    Added Login and Logout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220203609) do
+ActiveRecord::Schema.define(version: 20180221182449) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20180220203609) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
